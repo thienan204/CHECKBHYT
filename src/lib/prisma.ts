6c3805b6d38c6618918530ff931659e90dd21488
unknown <thienan204@gmail.com>
2026-02-12 16:54:03 +0700

    auto backup data

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+  return new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  })
 }
 
 declare global {
